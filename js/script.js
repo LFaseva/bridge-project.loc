cdb30c5810064ca64705b75d31380f617c421b73
LFaseva <Lfaseva@gmail.com>
2017-09-07 23:44:07 +0300

    Update script.js
    mobyle menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 window.onload = function(){
 
-    var btn_Menu = document.querySelector('.caption');
-    var mobile_Menu = document.querySelector('.menuBlock');
-
-    btn_Menu.onclick = function(){
-        mobile_Menu.classList.toggle('openMenu');
+  var btn_Menu = document.querySelector('.caption');
+    
+  for(var i=0; i<btn_Menu.length;i++){
+        btn_Menu[i].onclick = function(){
+            var mobile_Menu = this.parentNode.querySelector('.menuBlock');
+            mobile_Menu.classList.toggle('openMenu');
+        }
     }
 
 }
