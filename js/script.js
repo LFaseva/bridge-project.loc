24c9c41a42861b881079844fbb8357c6836b03a3
LFaseva <Lfaseva@gmail.com>
2017-09-07 23:49:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 
-  var btn_Menu = document.querySelector('.caption');
-    
-  for(var i=0; i<btn_Menu.length;i++){
+    var btn_Menu = document.querySelectorAll('.caption');
+
+    for(var i=0; i<btn_Menu.length;i++){
         btn_Menu[i].onclick = function(){
             var mobile_Menu = this.parentNode.querySelector('.menuBlock');
             mobile_Menu.classList.toggle('openMenu');
